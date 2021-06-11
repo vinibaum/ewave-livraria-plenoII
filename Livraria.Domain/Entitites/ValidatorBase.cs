241d5829ec46ca5b3e3dead014fa7defe6c6b5af
Vinícius Serraglio Baum <vinibaum@hotmail.com>
2021-06-11 08:25:37 -0400

    Criação de pastas internas dos projetos

---
+++ Livraria.Domain/Entitites/ValidatorBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Livraria.Domain.Entitites
+{
+    public class ValidatorBase
+    {
+        public IList<string> ListaErros { get; set; }
+        public ValidatorBase()
+        {
+            this.ListaErros = new List<string>();
+        }
+
+        public bool Valido { get => this.ListaErros.Count == 0; }
+        public void AdicionaErro(string erro)
+        {
+            this.ListaErros.Add(erro);
+        }
+    }
+}
---
+++ Livraria/Livraria.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Livraria.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
