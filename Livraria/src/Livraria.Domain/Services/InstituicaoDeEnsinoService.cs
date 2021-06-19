81999f3ef33f36cde8fb7fb74f8f80e972fc2b1c
Vinícius Serraglio Baum <vinibaum@hotmail.com>
2021-06-19 19:20:50 -0400

    Reorganização das pastas para que o compose docker consiga criar os containers

---
+++ Livraria/src/Livraria.Domain/Services/InstituicaoDeEnsinoService.cs
@@ -0,0 +1,97 @@
+﻿using Livraria.Domain.Entities;
+using Livraria.Domain.Entities.FolderInstituicaoDeEnsino;
+using Livraria.Domain.Interfaces.Repositories;
+using Livraria.Domain.Interfaces.Services;
+using Livraria.Domain.Interfaces.Services.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria.Domain.Services
+{
+    public class InstituicaoDeEnsinoService : IInstituicaoDeEnsinoService
+    {
+
+        private readonly IInstituicaoDeEnsinoRepository _instituicaoDeEnsinoRepository;
+
+        public InstituicaoDeEnsinoValidator _instituicaoValidator;
+
+        public IList<string> Erros { get; set; }
+
+        //public IList<string> Erros;
+
+
+        public InstituicaoDeEnsinoService(IInstituicaoDeEnsinoRepository InstituicaoDeEnsinoRepository, InstituicaoDeEnsinoValidator InstituicaoValidator)
+        {
+            _instituicaoDeEnsinoRepository = InstituicaoDeEnsinoRepository;
+            _instituicaoValidator = InstituicaoValidator;
+            Erros = new List<string>();
+        }
+
+        public async Task Save(InstituicaoDeEnsinoDto dto)
+        {
+            if (_instituicaoValidator.ValidarSave(dto))
+            {
+                var entity = new InstituicaoDeEnsino
+                {
+                    Nome = dto.Nome,
+                    Endereco = dto.Endereco,
+                    CNPJ = dto.CNPJ
+                };
+                await _instituicaoDeEnsinoRepository.Save(entity);
+            }
+            else
+            {
+                Erros =_instituicaoValidator.ListaErros;
+            }
+        }
+
+        public async Task Update(int id, InstituicaoDeEnsinoDto dto)
+        {
+            if (_instituicaoValidator.ValidarUpdate(id, dto))
+            {
+                var entity = _instituicaoDeEnsinoRepository.GetById(id);
+                entity.Nome = dto.Nome;
+                entity.Endereco = dto.Endereco;
+                entity.CNPJ = dto.CNPJ;
+                await _instituicaoDeEnsinoRepository.Update(entity);
+            }
+            else
+            {
+                Erros = _instituicaoValidator.ListaErros;
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            if (_instituicaoValidator.ValidarDelete(id))
+                await _instituicaoDeEnsinoRepository.Delete(_instituicaoDeEnsinoRepository.GetById(id));
+        }
+
+        public InstituicaoDeEnsino GetById(int InstituicaoDeEnsinoId)
+        {
+            return _instituicaoDeEnsinoRepository.GetById(InstituicaoDeEnsinoId);
+        }
+
+        public IEnumerable<InstituicaoDeEnsino> ObterTodos()
+        {
+            return _instituicaoDeEnsinoRepository.GetAll();
+        }
+
+        //public Task<IEnumerable<InstituicaoDeEnsino> ObterTodosAsync()
+        //{
+        //    return _instituicaoDeEnsinoRepository.GetAll();
+        //    var productList = await _productRepository.GetProductListAsync();
+        //    var mapped = ObjectMapper.Mapper.Map<IEnumerable<ProductDto>>(productList);
+        //    return mapped;
+        //}
+
+
+        public void Dispose()
+        {
+            //_instituicaoDeEnsinoRepository.Dispose();
+        }
+
+    }
+}
---
+++ Livraria/src/Livraria.Domain/Services/LivroService.cs
@@ -0,0 +1,176 @@
+﻿using Livraria.Domain.Entities;
+using Livraria.Domain.Entities.Base;
+using Livraria.Domain.Entities.FolderLivro;
+using Livraria.Domain.Entities.FolderUsuarioLivroEmprestado;
+using Livraria.Domain.Interfaces.Repositories;
+using Livraria.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria.Domain.Services
+{
+    public class LivroService : ILivroService
+    {
+
+        private readonly ILivroRepository _livroRepository;
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IUsuarioLivroEmprestadoRepository _usuarioLivroEmprestadoRepository;
+        private LivroValidator _livroValidator;
+
+        public IList<string> Erros { get; set; }
+
+        public LivroService(ILivroRepository livroRepository,
+                            IUsuarioRepository usuarioRepository,
+                            IUsuarioLivroEmprestadoRepository usuarioLivroEmprestadoRepository,
+                            LivroValidator livroValidator)
+        {
+            _livroRepository = livroRepository;
+            _usuarioLivroEmprestadoRepository = usuarioLivroEmprestadoRepository;
+            _livroValidator = livroValidator;
+            _usuarioRepository = usuarioRepository;
+            Erros = new List<string>();
+        }
+
+        public async Task Save(LivroDto dto)
+        {
+            if ( _livroValidator.ValidarSave(dto))
+            {
+                var livro = new Livro
+                {
+                    Titulo = dto.Titulo,
+                    Genero = dto.Genero,
+                    Publicacao = dto.Publicacao,
+                    Paginas = dto.Paginas,
+                    Autor = dto.Autor,
+                    Editora = dto.Editora,
+                    Descricao = dto.Descricao,
+                    IdUsuarioReserva = dto.IdUsurarioReserva
+                };
+                await _livroRepository.Save(livro);
+            }
+            else
+            {
+                Erros = _livroValidator.ListaErros;
+            }
+        }
+
+        public async Task Update(int id, LivroDto dto)
+        {
+            if ( _livroValidator.ValidarUpdate(id, dto))
+            {
+                var livro = _livroRepository.GetById(id);
+                livro.Titulo = dto.Titulo;
+                livro.Genero = dto.Genero;
+                livro.Publicacao = dto.Publicacao;
+                livro.Paginas = dto.Paginas;
+                livro.Autor = dto.Autor;
+                livro.Editora = dto.Editora;
+                livro.Descricao = dto.Descricao;
+                await _livroRepository.Update(livro);
+            }
+            else
+            {
+                Erros = _livroValidator.ListaErros;
+            }
+        }
+
+        public async Task Emprestar(int idLivro, int idUsuario)
+        {
+            if ( _livroValidator.ValidarEmprestimo(idLivro, idUsuario))
+            {
+                var livro = _livroRepository.GetById(idLivro);
+                livro.EmprestarLivro();
+                var usuarioLivroEmprestado = new UsuarioLivroEmprestado
+                {
+                    IdLivro = idLivro,
+                    IdUsuario = idUsuario,
+                    DataEmprestimo = DateTime.Now
+                };
+                await _livroRepository.Update(livro);
+                await _usuarioLivroEmprestadoRepository.Save(usuarioLivroEmprestado);
+            }
+            else
+            {
+                Erros = _livroValidator.ListaErros;
+            }
+        }
+
+        public async Task Devolver(int idLivro, int idUsuario)
+        {
+            if ( _livroValidator.ValidarDevolucao(idLivro, idUsuario))
+            {
+                var livro = _livroRepository.GetById(idLivro);
+                var usuario = _usuarioRepository.GetById(idUsuario);
+                var usuarioLivroEmprestado = usuario.LivrosEmprestados.Where(x => x.IdLivro == livro.Id && x.IsDevolvido == false).FirstOrDefault();
+                usuarioLivroEmprestado.DevolverLivro();
+                livro.DevolverLivro();
+                await _livroRepository.Update(livro);
+                await _usuarioLivroEmprestadoRepository.Update(usuarioLivroEmprestado);
+            }
+            else
+            {
+                Erros = _livroValidator.ListaErros;
+            }
+        }
+
+        public async Task Reservar(int idLivro, int idUsuario)
+        {
+            if ( _livroValidator.ValidarReserva(idLivro, idUsuario))
+            {
+                var livro = _livroRepository.GetById(idLivro);
+                var usuario = _usuarioRepository.GetById(idUsuario);
+                livro.Reservar(usuario);
+                await _livroRepository.Update(livro);
+            }
+            else
+            {
+                Erros = _livroValidator.ListaErros;
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            if ( _livroValidator.ValidarDelete(id))
+                await _livroRepository.Delete(_livroRepository.GetById(id));
+            else
+            {
+                Erros = _livroValidator.ListaErros;
+            }
+        }
+
+        public Livro GetById(int Id)
+        {
+            return _livroRepository.GetById(Id);
+        }
+
+        public IEnumerable<Livro> ObterTodos()
+        {
+            return _livroRepository.GetAll();
+        }
+
+        public IEnumerable<Livro> ObterParaEmprestar()
+        {
+            return _livroRepository.GetEmprestimo();
+        }
+
+        public IEnumerable<Livro> ObterParaDevolver(int idUsuario)
+        {
+            return _livroRepository.GetDevolucao(idUsuario);
+        }
+
+        public IEnumerable<Livro> FiltrarTitulo(string filtro)
+        {
+            return _livroRepository.GetByFilter(filtro);
+        }
+
+        public void Dispose()
+        {
+           // _livroRepository.Dispose();
+        }
+
+
+    }
+}
